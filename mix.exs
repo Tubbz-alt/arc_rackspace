0898b2339e1f869bea343deaf516020a89ec67d1
Blake Kostner <btkostner@gmail.com>
2018-12-06 10:28:33 -0700

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Arc.Storage.Rackspace.Mixfile do
 
   defp deps do
     [
-      {:arc, "~> 0.8.0"},
+      {:arc, "~> 0.11.0"},
       {:poison, "~> 3.0", override: true},
       {:rackspace, github: "livehelpnow/ex-rackspace"}
     ]
